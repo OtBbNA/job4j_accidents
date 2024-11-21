d962eda60d02ae98f255567197c4c5667abe486d
OtBbNA <tosha.kostylev.97@mail.ru>
2024-11-21 16:58:10 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,13 +1,15 @@
 package ru.job4j.accidents.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping({"/", "/index"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+        model.addAttribute("user", "User Name");
         return "index";
     }
 }
